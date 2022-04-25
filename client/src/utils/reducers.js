5d4d5ae32b66847554f133d4f496b7ced5e7834b
Mike Gordon <vprmatrix55@gmail.com>
2022-04-24 19:16:08 -0700

    added global StoreProvider and useStoreContext

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,20 +1,41 @@
+// React Hook 
+import { useReducer } from 'react';
+
 import {
-    UPDATE_PRODUCTS,
-    UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-  } from "./actions";
-  
-  export const reducer = (state, action) => {
-    switch (action.type) {
-      // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
-      case UPDATE_PRODUCTS:
-        return {
-          ...state,
-          products: [...action.products],
-        };
-  
-      // if it's none of these actions, do not update state at all and keep things the same!
-      default:
-        return state;
-    }
-  };
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from "./actions";
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    // if it's none of these actions, do not update state at all and keep things the same!
+    default:
+      return state;
+  }
+};
+
+// function that will know how to take in our state and update it through our reducer() function
+export function useProductReducer(initialState) {
+    // React Hook imported at the top of the file
+    return useReducer(reducer, initialState);
+  }
